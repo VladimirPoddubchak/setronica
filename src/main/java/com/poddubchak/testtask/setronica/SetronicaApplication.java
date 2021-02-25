882611ce531d19b544ff5c8bea12bea1de73fb26
Vladimir <v.m.poddubchak@gmail.com>
2021-02-25 23:58:28 +1000

    initial commit

---
+++ src/main/java/com/poddubchak/testtask/setronica/SetronicaApplication.java
@@ -0,0 +1,13 @@
+package com.poddubchak.testtask.setronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetronicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poddubchak/testtask/setronica/SetronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poddubchak.testtask.setronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
